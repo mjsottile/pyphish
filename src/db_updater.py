290aaa04302e6f9643b92594bac6fba71985760c
Matthew Sottile <mjsottile@me.com>
2020-04-07 22:11:22 -0700

    initial

---
+++ src/db_updater.py
@@ -0,0 +1,17 @@
+"""
+Update the database to download any missing shows over all time from
+1980 to 2030.  This will stop being correct in around 10 years...
+"""
+import api
+
+db_file = "phishdb.pickle"
+
+db = api.load_database(db_file)
+
+for year in range(1980,2030):
+    #print(year)
+    shows = api.shows_for_year(db, year)
+
+    for show in shows:
+        api.parse_show(db, show)
+        api.save_datebase(db_file, db)
